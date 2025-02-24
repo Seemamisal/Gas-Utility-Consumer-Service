e60eb9dd5f911dffd44b9458712de8e63784b74f
Seemamisal <102429680+Seemamisal@users.noreply.github.com>
2025-02-24 11:41:38 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,1 +1,9 @@
 
+from django.urls import path
+from .views import ServiceRequestCreateView, ServiceRequestListView, ServiceRequestUpdateView
+
+urlpatterns = [
+    path('submit/', ServiceRequestCreateView.as_view(), name='submit_request'),
+    path('track/', ServiceRequestListView.as_view(), name='track_request'),
+    path('update/<int:pk>/', ServiceRequestUpdateView.as_view(), name='update_request'),
+]
